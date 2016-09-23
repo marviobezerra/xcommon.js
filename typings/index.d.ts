925f269f75f76b00483bcb00bb239a1147acbd1a
Márvio Bezerra <marvio.bezerra@hotmail.com>
2016-09-23 00:37:55 -0300

    working on package

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/hammerjs/index.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
