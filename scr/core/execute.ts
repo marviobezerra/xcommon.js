fc112afd47928d3ac2b86eaa89e138eb59bd69bd
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2016-10-07 16:00:01 -0300

    Working on execute

--- scr/core/execute.ts
+++ scr/core/execute.ts
@@ -7,6 +7,10 @@ export enum ExecuteMessageType {
 
 export class ExecuteMessageInternal {
 
+	constructor(error?: Error) {
+		this.AddException(error);
+	}
+
 	public MessageException: string[] = [];
 	public StackTracers: string[] = [];
 
@@ -20,24 +24,57 @@ export class ExecuteMessageInternal {
 }
 
 export class ExecuteMessage {
+	constructor(type: ExecuteMessageType, message: string, messageInternal?: ExecuteMessageInternal) {
+		this.Type = type;
+		this.Message = message;
+		this.MessageInternal = messageInternal || new ExecuteMessageInternal();
+	}
+
 	public Type: ExecuteMessageType;
 	public Message: string;
 	public MessageInternal: ExecuteMessageInternal;
 }
 
 export class Execute {
+	constructor(execute?: Execute) {
+		this.Merge(execute);
+	}
+
 	public HasErro: boolean = false;
 	public HasException: boolean = false;
 	public HasWarning: boolean = false
 	public Messages: ExecuteMessage[] = [];
 
 	public AddMessage(execute: Execute): void;
-	public AddMessage(type: ExecuteMessageType, message: string): void;
+	public AddMessage(messageType: ExecuteMessageType, message: string): void;
 	public AddMessage(erro: Error, message: string): void;
-	public AddMessage(type: ExecuteMessageType | Error | Execute, message?: string): void {
+	public AddMessage(info: ExecuteMessageType | Error | Execute, message?: string): void {
+		if (typeof info === "number") {
+			this.Messages.push(new ExecuteMessage(info, message));
+			return;
+		}
+
+		if (info instanceof Error) {
+			let internal: ExecuteMessageInternal = new ExecuteMessageInternal(info);
+			this.Messages.push(new ExecuteMessage(ExecuteMessageType.Error, message, internal));
+			return;
+		}
+
+		if (info instanceof Execute) {
+			this.Merge(info);
+			return;
+		}	
 	}
 
-	private CheckMessage(): void {
+	private Merge(execute: Execute): void {
+		if (!execute)
+			return;
+
+		this.Messages = [...this.Messages, ...execute.Messages];
+		this.CheckMessages();
+	}
+
+	private CheckMessages(): void {
 		if (!this.HasWarning)
 			this.HasWarning = this.Messages.any((c: ExecuteMessage) => c.Type == ExecuteMessageType.Warning);
 
@@ -48,3 +85,14 @@ export class Execute {
 			this.HasException = this.Messages.any((c: ExecuteMessage) => c.Type == ExecuteMessageType.Exception);
 	}
 }
+
+export class ExecuteT<T> extends Execute {
+	constructor()
+	constructor(execute?: Execute, entity?: T);
+	constructor(execute?: Execute);
+	constructor(entity?: T) {
+		super();
+		this.Entity = entity;
+	}
+	public Entity: T;
+} 
