885d40148389164262cafb21ddc3567a8bbc08ea
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2016-11-16 13:11:53 -0200

    Created gulp tasks to run unit tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,43 @@
 var gulp = require("gulp"),
     gutil = require("gulp-util"),
-    htmlmin = require("gulp-htmlmin"),
     livereload = require("gulp-livereload"),
     rimraf = require("gulp-rimraf"),
     webpack = require("webpack"),
     webpackStream = require("webpack-stream"),
     connect = require("gulp-connect"),
-    runSequence = require("run-sequence"),
-    replace = require("gulp-replace");
+    runSequence = require("run-sequence");
 
 var helper = {
     tasks: {
-        clear: "clear",
-        watch: "watch",
+        clear: "clear",        
+        test: {
+            watch: "test:watch",
+            server: "test:server",
+            compile: "test:compile"
+        },
         compile: {
             deploy: "compile:deploy",
-            server: "compile:server",
-            html: "compile:html",
-            ts: "compile:ts",
-            options: {
-                watch: false,
-                deploy: false
-            }
+            server: "compile:local"
         }
     },
     path: {
+        test: {
+            root: "./test/runner",
+            spec: "./test/runner/spec"
+        },
         source: {
-            defaultFile: "./app/App.Component.ts",
-            html: "./app/**/*.html"
-        },
-        destination: {
-            port: "8080",
-            html: "./public",
-            assets: "./public/assets"
+            root: "./scr",
+            lib: "./lib"
         }
-    },
-    htmlMimify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeTagWhitespace: false,
-        removeRedundantAttributes: true,
-        caseSensitive: true
-    },
-    reload: {
-        tag: function () {
-            return helper.tasks.compile.options.watch === true
-                ? "<script type=\"text/javascript\" src=\"//localhost:" + helper.reload.port + "/livereload.js?snipver =1\" async defer></script>"
-                : "";
-        },
-        port: 35719
     },
     webpack: function () {
         var result = Object.create(require("./webpack.config.js"));
 
-        if (helper.tasks.compile.options.watch === true) {
-            result.debug = true;            
-            result.devtool = "source-map";            
-            result.watch = true;
-            result.plugins = result.plugins || [];
-            result.plugins.push(helper.webPackLog);
-        }
-
-        if (helper.tasks.compile.options.deploy === true) {
-            result.plugins = result.plugins || [];
-            result.plugins.push(new webpack.NoErrorsPlugin());
-            result.plugins.push(new webpack.optimize.DedupePlugin());
-            result.plugins.push(new webpack.optimize.UglifyJsPlugin({
-                beautify: false,
-                comments: false,
-                minimize: true,
-                mangle: {
-                    screw_ie8: true,
-                    keep_fnames: true
-                },
-                compress: {
-                    warnings: false,
-                    screw_ie8: true,
-                    drop_console: true,
-                    drop_debugger: true
-                }
-            }));
-        }
+        result.debug = true;
+        result.devtool = "source-map";
+        result.watch = true;
+        result.plugins = result.plugins || [];
+        result.plugins.push(helper.webPackLog);
 
         return result;
     },
@@ -101,58 +58,40 @@ var helper = {
 };
 
 gulp.task(helper.tasks.clear, function () {
-    return gulp.src([helper.path.destination.html], { read: false })
+    return gulp.src([helper.path.source.lib, helper.path.test.spec], { read: false })
         .pipe(rimraf());
 });
 
-gulp.task(helper.tasks.compile.html, function () {
+gulp.task(helper.tasks.test.compile, function () {
 
-    return gulp.src([helper.path.source.html])
-        .pipe(replace("[{reload}]", helper.reload.tag()))
-        .pipe(htmlmin(helper.htmlMimify))
-        .pipe(gulp.dest(helper.path.destination.html))
-        .on("end", function () {
-            livereload.reload("index.html");
-        });
-});
-
-gulp.task(helper.tasks.compile.ts, function () {
-    return gulp.src(helper.path.source.defaultFile)
+    return gulp.src(helper.path.test.root + "index.html", { read: false })
         .pipe(webpackStream(helper.webpack()))
-        .pipe(gulp.dest(helper.path.destination.assets))
+        .pipe(gulp.dest(helper.path.test.spec))
         .pipe(livereload());
 });
 
-gulp.task(helper.tasks.compile.deploy, function () {
-
-    helper.tasks.compile.options.watch = false;
-    helper.tasks.compile.options.deploy = true;
-
-    return runSequence(helper.tasks.clear,
-        [helper.tasks.compile.html, helper.tasks.compile.ts]);
-});
-
-gulp.task(helper.tasks.compile.server, function () {
+gulp.task(helper.tasks.test.server, function () {
     livereload.listen({
-        port: helper.reload.port
+        port: 38082
     });
 
     connect.server({
-        root: helper.path.destination.html,
-        port: helper.path.destination.port,
-        fallback: helper.path.destination.html + "/index.html",
+        root: helper.path.test.root,
+        port: 38081,
+        fallback: helper.path.test.root + "/index.html",
         livereload: false
     });
 })
 
-gulp.task(helper.tasks.watch, function () {
+gulp.task(helper.tasks.test.watch, function () {
 
-    helper.tasks.compile.options.debug = true;
-    helper.tasks.compile.options.watch = true;
-    helper.tasks.compile.options.deploy = false;
+    gulp.watch([helper.path.source.lib, helper.path.test.spec], [helper.tasks.compile.test]);
 
-    gulp.watch([helper.path.source.html], [helper.tasks.compile.html]);
-
-    return runSequence(helper.tasks.clear,
-        [helper.tasks.compile.html, helper.tasks.compile.ts, helper.tasks.compile.server]);
+    return runSequence(
+        helper.tasks.clear,
+        [
+            helper.tasks.test.compile,
+            helper.tasks.test.server
+        ]
+    );
 });
--- test/core/simple.spec.ts
+++ test/core/simple.spec.ts
@@ -3,4 +3,14 @@ describe("Simple test", () => {
 		let x: number = 1;
 		expect(1).toBe(1);
 	});
+
+	it("Teste 2", () => {
+		let x: number = 1;
+		expect(1).toBe(1);
+	});
+
+	it("Teste work!!!", () => {
+		let x: number = 1;
+		expect(1).toBe(1);
+	});
 });
