3dc551f6c74f7fd5215ca6311b0b551093e61e06
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2017-11-16 23:03:37 -0500

    Export entity

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { AutoFormService } from "./services/auto-form.service";
 export { HttpUtilService } from "./services/http-util.service";
 export { XCommonModule } from "./xcommon.module";
+export * from "./entity/entity";
