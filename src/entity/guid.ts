26facc1f889a0a8de1f8bd9d8e52543a0aab2aa3
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2017-12-18 00:16:52 -0500

    Update to angular 5

--- src/entity/guid.ts
+++ src/entity/guid.ts
@@ -1,10 +1,19 @@
 export class Guid {
 	public static NewGuid(): string {
-		return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-			// tslint:disable
-			var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
-			return v.toString(16);
-		});
+		let result: string;
+		let i: string;
+		let j: number;
+
+		result = "";
+		for (j = 0; j < 32; j++) {
+			if (j === 8 || j === 12 || j === 16 || j === 20) {
+				result = result + "-";
+			}
+
+			i = Math.floor(Math.random() * 16).toString(16).toUpperCase();
+			result = result + i;
+		}
+		return result;
 	}
 
 	public static Empty(): string {
